f3e1cf1d911660be74974bee293cd26f8536dba0
linkerx <diego.martinezdiaz@linkerx.com.ar>
2017-12-21 00:52:54 -0300

    desc fullimage

--- fullscreenImage/fullmodal.js
+++ fullscreenImage/fullmodal.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var FontAwesome = require('react-fontawesome');
+var renderHTML = require('react-render-html');
 
 module.exports = {
   openFull: function(modalContainer,imageSrc,desc){
@@ -12,7 +13,7 @@ module.exports = {
           <img src={imageSrc} />
         </div>
         <div className='descripcion' >
-          <span>{desc}</span>
+          <span>{renderHTML(desc)}</span>
         </div>
         <div className='close-trigger' onClick={() => {ReactDOM.render(<div></div>,document.getElementById(modalContainer))}} >
           <FontAwesome name='compress' />
