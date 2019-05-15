1b7b389c1d71d8fa378040d19a3a318c325be556
Diego Martinez Diaz <diego.martinezdiaz@linkerx.com.ar>
2019-05-15 20:25:35 -0300

    estilos base en articulo

--- site/archive/index.js
+++ site/archive/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import WpList from '../../list';
-import './styles.less';
+import './styles.scss';
 
 function WpSiteArchive(props){
 
--- site/post/index.js
+++ site/post/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import WpItem from '../../item';
-import './styles.less';
+import './styles.scss';
 
 function WpSitePost(props){
   return (
     <section id='site-post' className={props.site+"-"+props.type}>
-      <WpItem site={props.site} ready={props.ready} type={props.type} slug={props.slug} img_size='full' debug={false} />
+      <WpItem site={props.site} ready={props.ready} type={props.type} slug={props.slug} img_size='full' debug={false} heading='1' />
     </section>
   )
 }
