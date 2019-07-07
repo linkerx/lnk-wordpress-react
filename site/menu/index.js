febc68909268e4f8a8736ee92a561f22c4aeb848
linkerx <diego.martinezdiaz@linkerx.com.ar>
2019-07-06 22:53:28 -0300

    posicion menu sitio distinta a main

--- site/menu/index.js
+++ site/menu/index.js
@@ -5,7 +5,7 @@ import 'styles/wp/site-menu.scss';
 class WpSiteMenu extends React.Component {
     render(){
         return <div id='wp-site-menu'>
-            <WpMenu site={this.props.site} location='main-menu-location' />
+            <WpMenu site={this.props.site} location='site-menu-location' />
         </div>
     }
 }
