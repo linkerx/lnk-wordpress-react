bbd8aada8281814eb61e1d2b3514ec044a913a69
fkmurphy <njmdistrisoft@gmail.com>
2019-08-13 17:52:38 -0300

    fix ===

--- slider.js
+++ slider.js
@@ -116,7 +116,7 @@ class WpSlider extends React.Component {
   isActive(id) {
     if(this.state.current){
       return this.state.current.items.find(function(item) {
-          return item.id == id;
+          return item.id === id;
       })
     }
     return false;
@@ -155,7 +155,7 @@ class WpSlider extends React.Component {
 
   lastPage(){
     var response = Math.floor(this.state.options.listCount / this.state.options.pageCount);
-    if(((this.state.options.listCount / this.state.options.pageCount) - response) == 0) {
+    if(((this.state.options.listCount / this.state.options.pageCount) - response) === 0) {
       return response;
     } else {
       return response + 1;
@@ -163,7 +163,7 @@ class WpSlider extends React.Component {
   }
 
   nextPage(){
-    if(this.state.current.page == this.lastPage()) {
+    if(this.state.current.page === this.lastPage()) {
       return 1;
     } else {
       return this.state.current.page + 1;
@@ -171,7 +171,7 @@ class WpSlider extends React.Component {
   }
 
   backPage(){
-    if(this.state.current.page == 1) {
+    if(this.state.current.page === 1) {
       return this.lastPage();
     } else {
       return this.state.current.page - 1;
@@ -213,7 +213,7 @@ class WpSlider extends React.Component {
           {
             Array.apply(null,{length: this.lastPage()}).map(Number.call,Number).map(function(item, index){
               var activeClass = 'inactive';
-              if(this.state.current && this.state.current.page == item+1){
+              if(this.state.current && this.state.current.page === item+1){
                 activeClass = 'active';
               }
 
